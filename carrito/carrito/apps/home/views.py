2726b5c13f977c1872e4da4dcc42ff94b6961b20
sheolseol <siolsiol@hotmail.com>
2013-05-26 17:25:49 -0400

    Mensaje enviado por ctx desde vista about a about.html

--- carrito/carrito/apps/home/views.py
+++ carrito/carrito/apps/home/views.py
@@ -7,4 +7,6 @@ def index_view(request):
 	return render_to_response('home/index.html',context_instance=RequestContext(request))
 
 def about_view(request):
-	return render_to_response('home/about.html',context_instance=RequestContext(request))
+	mensaje = 'Mensaje enviado desde mi vista'
+	ctx = {'msg':mensaje}
+	return render_to_response('home/about.html',ctx,context_instance=RequestContext(request))
